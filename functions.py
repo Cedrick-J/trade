1bd6f8518b86c391826c303461f1294dc3aebb95
Cedrick <70344513+HaberTech@users.noreply.github.com>
2021-05-03 22:26:52 +0300

    update functions.py

--- functions.py
+++ functions.py
@@ -53,6 +53,6 @@ def makeBid(typ):
 def placeBid(typ, bid):
     log = open("log.txt","a") 
     log.write("\nBid => "+str(bid));
-    output = subprocess.check_output(["php", "script.php", '{"op":"placeBid", "val":"https://app.irontrade.com/ajax/openOperation?type='+str(typ)+'&stock=EURUSD&interval=900&amount='+str(bid)+'accountId=4739813"}']).decode()
+    output = subprocess.check_output(["php", "script.php", '{"op":"placeBid", "val":"https://app.irontrade.com/ajax/openOperation?type='+str(typ)+'&stock=EURUSD&interval=3600&amount='+str(bid)+'accountId=4739813"}']).decode()
     log.write('\n'+output+'\n') 
     log.close()
