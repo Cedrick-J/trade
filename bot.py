3b5d862c592ea3145146867e0031023c4ab65d60
Cedrick <70344513+HaberTech@users.noreply.github.com>
2021-05-03 22:25:11 +0300

    update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import datetime
 from time import sleep
 from functions import *
 
@@ -8,9 +9,9 @@ while True:
     print()
     lastPrediction = prediction
     log = open("log2.txt","a") 
-    e = datetime.datetime.now()
-    log.write('\n\n'+e.strftime("%Y-%m-%d %H:%M:%S")+':\n'+"\Prediction :"+prediction+
+    date = datetime.datetime.now();
+    log.write('\n\n'+date.strftime("%Y-%m-%d %H:%M:%S")+':\n'+"\Prediction :"+prediction+
     "\nlastPrediction => "+lastPrediction)
-    log.close()
+    log.close();
     checkDirection(prediction);
     sleep(30)
