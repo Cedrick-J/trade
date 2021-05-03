90deb39e5f85c5c1a1c350c8b56988c77eb71af1
Cedrick <70344513+HaberTech@users.noreply.github.com>
2021-05-03 17:50:30 +0300

    update bot.py

--- bot.py
+++ bot.py
@@ -3,20 +3,12 @@ from functions import *
 
 lastPrediction = ""
 while True:
-    pred1min = getPrediction('60')
-    pred5min = getPrediction('300')
-    pred15min = getPrediction('900')
+    prediction = getPrediction('60')
+
     print("\n1 Min => "+pred1min)
-    print("5 Min => "+pred5min)
-    print("15 Min =>"+pred15min)
-    print("Last => "+lastPrediction)
+    print("lastPrediction => "+lastPrediction)
     
-#    if pred1min==pred5min:
-#    if pred1min != lastPrediction:
-#        lastPrediction = pred1min
-#        checkDirection(pred1min);
-    if pred15min != lastPrediction:
-        lastPrediction = pred15min
-        checkDirection(pred15min);
-    else: print("Waiting")
+
+    lastPrediction = prediction
+    checkDirection(prediction);
     sleep(30);
