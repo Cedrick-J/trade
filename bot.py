92714865c302e42d7d4a8c861a31363429f40489
Cedrick <70344513+HaberTech@users.noreply.github.com>
2021-05-03 22:25:41 +0300

    update bot.py

--- bot.py
+++ bot.py
@@ -5,13 +5,13 @@ from functions import *
 lastPrediction = ""
 while True:
     prediction = getPrediction()
-
-    print()
     lastPrediction = prediction
-    log = open("log2.txt","a") 
     date = datetime.datetime.now();
+
+    log = open("log2.txt","a") 
     log.write('\n\n'+date.strftime("%Y-%m-%d %H:%M:%S")+':\n'+"\Prediction :"+prediction+
     "\nlastPrediction => "+lastPrediction)
     log.close();
+    
     checkDirection(prediction);
     sleep(30)
